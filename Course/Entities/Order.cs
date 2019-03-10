078fd477a63f49d343ff85e7c9ed25ac1deef1ad
Felipe Tadeu Geraldo <felipe.develop@gmail.com>
2019-03-10 11:32:55 -0300

    adicionando exercicio de enumeracao e composicao curso c#

---
+++ Course/Entities/Order.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Course.Entities.Enums;
+using System.Globalization;
+
+namespace Course.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; } = new Client();
+        public List<OrderItem> OrderItens { get; set; } = new List<OrderItem>();
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem Item)
+        {
+            OrderItens.Add(Item);
+        }
+
+        public void RemoveItem(OrderItem Item)
+        {
+            OrderItens.Remove(Item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+
+            foreach (OrderItem item in OrderItens)
+            {
+                sum += item.SubTotal();
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("Order moment: ");
+            stringBuilder.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            stringBuilder.Append("Order status: ");
+            stringBuilder.AppendLine(Status.ToString());
+            stringBuilder.Append("Client: ");
+            stringBuilder.AppendLine(Client.ToString());
+            stringBuilder.AppendLine("Order items:");
+            foreach (OrderItem item in OrderItens)
+            {
+                stringBuilder.AppendLine(item.ToString());
+            }
+            stringBuilder.Append("Total price: $");
+            stringBuilder.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+           
+            return stringBuilder.ToString();
+        }
+
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client c = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status (PendingPayment/Processing/Shipped/Delivered): ");
+            OrderStatus os = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, os, c);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product p = new Product(productName, productPrice);
+
+                OrderItem item = new OrderItem(quantity, productPrice, p);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
